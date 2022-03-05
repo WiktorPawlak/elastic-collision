21b6153321a658186d4492f5491e722b858b6030
236621 Wiktor Pawlak <236621@edu.p.lodz.pl>
2022-03-05 01:54:48 +0100

    Added sample program

---
+++ ConcurrentProgramming/Fraction.cs
@@ -0,0 +1,25 @@
+﻿namespace ConcurrentProgramming
+{
+    public class Fraction
+    {
+        private readonly int _dividend;
+        private readonly int _divisor;
+
+        public Fraction(int dividend, int divisor)
+        {
+            _dividend = dividend;
+            _divisor = divisor;
+        }
+
+        public double Evaluate() => (double)_dividend / _divisor;
+        public override string ToString() => $"{_dividend}/{_divisor}";
+    }
+
+    public class Program
+    {
+        static void Main()
+        {
+            System.Console.WriteLine("Simple fraction evaluator!");
+        }
+    }
+}
