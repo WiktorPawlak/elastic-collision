e0c019d2112a689c3314661bf34d64c76c2706e9
Antoni Jończyk <236551@edu.p.lodz.pl>
2022-04-13 00:45:53 +0200

    mor vectors

--- ElasticCollision/Data/Vector.cs
+++ ElasticCollision/Data/Vector.cs
@@ -3,18 +3,37 @@ public class Vector
     public double x { get; }
     public double y { get; }
 
-    public Vector(double X, double Y)
+    private Vector(double X, double Y)
     {
         x = X;
         y = Y;
     }
-
+    public override bool Equals(object other)
+    {
+        var o = other as Vector;
+        if (o == null)
+        {
+            return false;
+        }
+        else
+        {
+            return (x == o.x) && (y == o.y);
+        }
+    }
     // ╔═════════╗
     // ║ funkcje ║
     // ╚═════════╝
 
-    public static Vector add(Vector a, Vector b)
+    public static Vector vec(double x, double y)
     {
-        return a;
+        return new Vector(x, y);
     }
+
+    public static Vector operator +(Vector l, Vector r)
+    {
+        var x = l.x + r.x;
+        var y = l.y + r.y;
+        return vec(x, y);
+    }
+
 }
--- ElasticCollision/DataTest/VectorTest.cs
+++ ElasticCollision/DataTest/VectorTest.cs
@@ -1,19 +1,30 @@
 using System;
 using Xunit;
+using static Vector;
 
 namespace DataTest
 {
     public class VectorTest
     {
+
         [Fact]
         public void TestCreation()
         {
-            Vector v1 = new Vector(0, 0);
+            Vector v1 = vec(0, 0);
             Assert.Equal(0, v1.x);
             Assert.Equal(0, v1.y);
-            Vector v2 = new Vector(5, -9);
+            Vector v2 = vec(5, -9);
             Assert.Equal(5, v2.x);
             Assert.Equal(-9, v2.y);
+        }
+        [Fact]
+        public void TestAddition()
+        {
+            var a = vec(0, 0) + vec(0, 0);
+            Assert.Equal(0, a.x);
+            Assert.Equal(0, a.y);
+            var b = vec(5, 5) + vec(5, 5);
+            Assert.Equal(vec(10, 10), b);
         }
     }
 }
