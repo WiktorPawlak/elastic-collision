8a6ef5109f6f1e5a99f111a37e1eea49038e9736
Antoni Jończyk <236551@edu.p.lodz.pl>
2022-04-14 19:56:55 +0200

    managed to get fscking "language feature" working

--- ElasticCollision/Data/Ball.cs
+++ ElasticCollision/Data/Ball.cs
@@ -1,12 +1,10 @@
 namespace ElasticCollision.Data
 {
 
-    public record Ball
-    {
-
-        public double radius { get; }
-        public double mass { get; }
-        public Vector location { get; }
-        public Vector velocity { get; }
-    }
+    public record Ball(
+         double radius,
+         double mass,
+         Vector location,
+         Vector velocity
+    );
 }
--- ElasticCollision/DataTest/BallTest.cs
+++ ElasticCollision/DataTest/BallTest.cs
@@ -9,11 +9,11 @@ namespace DataTest
         [Fact]
         public void TestModificationWorksTheWayIThinkItWorks()
         {
-             // Ball a = new(10, 20, vec(1, 2), vec(3, 4));
-             // var b = a with { location = vec(10, 20) };
-             // Assert.Equal(vec(1, 2), a.location);
-             // Assert.Equal(vec(10, 20), b.location);
-             // Assert.Equal(vec(3, 4), b.velocity);
+            Ball a = new(10, 20, vec(1, 2), vec(3, 4));
+            var b = a with { location = vec(10, 20) };
+            Assert.Equal(vec(1, 2), a.location);
+            Assert.Equal(vec(10, 20), b.location);
+            Assert.Equal(vec(3, 4), b.velocity);
 
         }
 
