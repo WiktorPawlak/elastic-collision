0b736bdcbbf07e8311e1804302c95d293775d2b8
Antoni Jończyk <236551@edu.p.lodz.pl>
2022-04-15 19:15:12 +0200

    + nie wiem co tam się działo, wizja api

--- ElasticCollision/Logic/LogicAPI.cs
+++ ElasticCollision/Logic/LogicAPI.cs
@@ -1,64 +1,21 @@
 ﻿using System;
-using System.Collections.ObjectModel;
-using System.Collections.Specialized;
-using System.Linq;
 
 namespace ElasticCollision.Logic
 {
     public abstract class LogicAPI
     {
-        private ObservableCollection<Ball> _balls;
-        public abstract void CreateBall(double radius, double mass, Vector loc, Vector velocity);
-        public static LogicAPI CreateCollisionLogic(DataAPI data = default)
-        {
-            return new CollisionLogic(data == null ? DataAPI.CreateBallData() : data);
-        }
+        public abstract WorldState GetState();
 
-        private class CollisionLogic : LogicAPI
-        {
-            private readonly DataAPI _ballData;
+        // WorldWatcher dostaje nowy stan świata w każdej klatce
+        public delegate void WorldWatcher(WorldState state);
+        public abstract void AddWatcher(WorldWatcher del);
 
-            public CollisionLogic(DataAPI dataLayerAPI)
-            {
-                _ballData = dataLayerAPI;
-                _balls = new ObservableCollection<Ball>();
-                _balls.CollectionChanged += CollectionChangedHandler;
-            }
-
-            public override void CreateBall(double radius, double mass, Vector loc, Vector velocity)
-            {
-                Ball ball = new(radius, mass, loc, velocity);
-                //if (this._collisionSolver.IsPositionOccupied(ball))
-                //{
-                //    throw new ArgumentException("Position is already ocupied.");
-                //}
-                _balls.Add(ball);
-            }
-
-            private void CollectionChangedHandler(object sender,
-                  NotifyCollectionChangedEventArgs notifyCollectionChangedEventArgs)
-            {
-                foreach (Ball changedObject in notifyCollectionChangedEventArgs.NewItems.Cast<Ball>())
-                {
-                    switch (notifyCollectionChangedEventArgs.Action)
-                    {
-                        case NotifyCollectionChangedAction.Add:
-                            this.CanvasMain.Children.Add(changedObject.WpfShape);
-                            break;
-                        case NotifyCollectionChangedAction.Remove:
-                            //this.CanvasMain.Children.Remove(changedObject.WpfShape);
-                            break;
-                        case NotifyCollectionChangedAction.Replace:
-                            break;
-                        case NotifyCollectionChangedAction.Move:
-                            break;
-                        case NotifyCollectionChangedAction.Reset:
-                            break;
-                        default:
-                            throw new ArgumentOutOfRangeException();
-                    }
-                }
-            }
-        }
+        public abstract void StartSimulation();
+        public abstract void StopSimulation();
+        // może jeszcze jakieś kontrolki do FPS świata,
+        // bo ΔT będzie raczej zakodowana na sztywno
+        public abstract void CreateBall(Ball newOne);
+        // we ball, i tak musielibyśmy korzystać z `Vector`
+        // ewentualnie dać tutaj (x, y, ɸ)
     }
 }
---
+++ ElasticCollision/Logic/Wall.cs
@@ -0,0 +1,8 @@
+using System;
+namespace ElasticCollision.Logic
+{
+    public record Wall(
+         Vector Point1,
+         Vector Point2
+    );
+}
