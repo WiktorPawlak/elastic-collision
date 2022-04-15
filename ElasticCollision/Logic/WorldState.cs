10f92d58e6d68a82d5728549ebf5a28a9e04b3ad
Antoni Jończyk <236551@edu.p.lodz.pl>
2022-04-16 01:04:33 +0200

    + some state

--- ElasticCollision/Logic/WorldState.cs
+++ ElasticCollision/Logic/WorldState.cs
@@ -1,9 +1,21 @@
-using System;
-using System.Collections.Immutable;
+using System.Collections.Generic;
+using System.Linq;
 namespace ElasticCollision.Logic
 {
     public record WorldState(
-         ImmutableList<Ball> Balls,
+         IEnumerable<Ball> Balls,
          Area area
-    );
+    )
+    {
+        public WorldState Move(double Δt)
+        {
+            var newBalls = Balls.Select(ball => ball.Budge(Δt).Collide(area));
+            // tutaj będzie kod zderzający piłeczki
+            // zasadniczo trzeba podzielić je na dwie kolekcje
+            // > piłeczki które nie zderzyły się z innymi piłeczkami
+            // > pary przecinających się piłeczek
+            // te drugie zderzyć i dodać obie grupy
+            return this with { Balls = newBalls };
+        }
+    }
 }
