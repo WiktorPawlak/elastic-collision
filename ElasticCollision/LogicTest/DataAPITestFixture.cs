694457a8b8185114f48d15f010b13825324d24c2
236621 Wiktor Pawlak <236621@edu.p.lodz.pl>
2022-04-21 19:14:14 +0200

    Add LogicAPI tests

---
+++ ElasticCollision/LogicTest/DataAPITestFixture.cs
@@ -0,0 +1,13 @@
+﻿using ElasticCollision.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicTest
+{
+    internal class DataAPITestFixture : DataAPI
+    {
+    }
+}
---
+++ ElasticCollision/LogicTest/LogicAPITest.cs
@@ -0,0 +1,31 @@
+﻿using ElasticCollision.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LogicTest
+{
+    public class LogicAPITest
+    {
+        private readonly LogicAPI _logicAPITest = LogicAPI.CreateCollisionLogic(500,
+            500, new DataAPITestFixture());
+
+        [Fact]
+        public void TestCreateBallNegative()
+        {
+            _logicAPITest.StartSimulation();
+            Ball ball = new(5, 5, Vector.vec(0, 0), Vector.vec(0, 0));
+            Assert.Throws<Exception>(() => _logicAPITest.CreateBall(ball));
+        }
+
+        [Fact]
+        public void TestAddBallsNegative()
+        {
+            _logicAPITest.StartSimulation();
+            Assert.Throws<Exception>(() => _logicAPITest.AddBalls(10, 10, 10));
+        }
+    }
+}
