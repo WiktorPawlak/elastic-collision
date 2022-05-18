a7829c302fe671325679fe2adce0d88d1c507338
236621 Wiktor Pawlak <236621@edu.p.lodz.pl>
2022-05-19 00:53:22 +0200

    Refactor Tests

---
+++ ElasticCollision/DataTest/DataAPITest.cs
@@ -0,0 +1,27 @@
+﻿using ElasticCollision.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DataTest
+{
+    public class DataAPITest
+    {
+        private DataAPI sub = DataAPI.CreateBallData();
+
+        [Fact]
+        public void TestAddingBalls()
+        {
+            Assert.Empty(sub.GetState().Balls);
+            sub.AddBalls(1, 5, 6);
+            Assert.Equal(5, sub.GetState().Balls.First().Radius);
+            Assert.Equal(6, sub.GetState().Balls.First().Mass);
+            Assert.Single(sub.GetState().Balls);
+            sub.AddBalls(10, 1, 1);
+            Assert.Equal(11, sub.GetState().Balls.Count());
+        }
+    }
+}
--- ElasticCollision/LogicTest/LogicAPITest.cs
+++ ElasticCollision/LogicTest/LogicAPITest.cs
@@ -14,53 +14,14 @@ namespace LogicTest
     {
         private readonly LogicAPI sub = LogicAPI.CreateCollisionLogic(new DataAPITestFixture());
 
-        [Fact]
-        public void TestAddingBallsWhileRunning()
-        {
-            sub.StartSimulation();
-            sub.AddBalls(10, 10, 10);
-            Assert.Equal(10, sub.GetCurrentState().Balls.Count());
-            Thread.Sleep(30);
-            Assert.Equal(10, sub.GetCurrentState().Balls.Count());
-        }
-        [Fact]
-        public void TestAddingBalls()
-        {
-            Assert.Empty(sub.GetCurrentState().Balls);
-            sub.AddBalls(1, 5, 6);
-            Assert.Equal(5, sub.GetCurrentState().Balls.First().Radius);
-            Assert.Equal(6, sub.GetCurrentState().Balls.First().Mass);
-            Assert.Single(sub.GetCurrentState().Balls);
-            sub.AddBalls(10, 1, 1);
-            Assert.Equal(11, sub.GetCurrentState().Balls.Count());
-            sub.StartSimulation();
-            sub.StopSimulation();
-            sub.AddBalls(1, 1, 1);
-            Assert.Equal(12, sub.GetCurrentState().Balls.Count());
-        }
-        [Fact]
-        public void testSimulationSimple()
-        {
-            sub.AddBalls(10, 10, 10);
-            var old = sub.GetCurrentState();
-            Assert.Equal(old, sub.GetCurrentState());
-            sub.NextTick();
-            Assert.Equal(10, sub.GetCurrentState().Balls.Count());
-            Assert.NotEqual(old, sub.GetCurrentState());
-
-        }
-
         private class CallCounter
         {
             public int count;
-            public WorldState last;
-            public void Update(WorldState state)
+            public void Update(List<BallLogic> state)
             {
                 count++;
-                last = state;
             }
         }
-
         [Fact]
         public void TestSimulationHappens()
         {
@@ -79,8 +40,6 @@ namespace LogicTest
             Thread.Sleep(10);
             Assert.True(ctr.count > 2);
             Assert.True(50 > ctr.count); // aż tak szybko nie chcemy
-            Assert.Equal(sub.GetCurrentState(), ctr.last);
-
         }
 
     }
